e74dce8347108d898cbac4899e8a9235761a201e
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-07-30 12:46:47 +0200

    implement endpoints to Read, Update and Delete on DB

--- app.js
+++ app.js
@@ -84,40 +84,81 @@ app.post("/pizzas", (req, res, next) => {
 })
 
 
-// GET /pizzas
-// GET /pizzas?maxPrice=xx
+// GET /pizzas -- Get the list of pizzas
+// GET /pizzas?maxPrice=20
 app.get("/pizzas", (req, res, next) => {
 
     const { maxPrice } = req.query
 
-    // if maxPrice is undefined, return an array with all the pizzas
-    if(maxPrice === undefined) {
-        res.json(pizzasArr)
-        return
+    let filter = {}
+
+    if (maxPrice !== undefined) {
+        filter = { price: { $lte: maxPrice } }
     }
 
-    // if we have maxPrice, then we return only the pizzas with that maxPrice
-    const filteredPizzas = pizzasArr.filter((pizzaObj) => {
-        return pizzaObj.price <= parseFloat(maxPrice);
-    })
-
-    res.json(filteredPizzas)
+    Pizza.find(filter)
+        .then((pizzasFromDB) => {
+            res.json(pizzasFromDB)
+        })
+        .catch(err => {
+            console.log("Error getting pizzas from DB...")
+            console.log(err)
+            res.status(500).json({ error: "Failed to get list of pizzas" })
+        })
 })
 
 
-
-// GET /pizzas/xxx
+// GET /pizzas/:pizzaId -- Get the details for one pizza
 app.get("/pizzas/:pizzaId", (req, res, next) => {
 
-    let { pizzaId } = req.params // note: we get pizzaId as a string
+    const { pizzaId } = req.params
 
-    pizzaId = parseInt(pizzaId) // convert to an integer
+    Pizza.findById(pizzaId)
+        .then((pizzaFromDB) => {
+            res.json(pizzaFromDB)
+        })
+        .catch(error => {
+            console.log("Error getting pizza details from DB...");
+            console.log(error);
+            res.status(500).json({ error: "Failed to get pizza details" });
+        })
+})
 
-    const result = pizzasArr.find((pizzaObj, i, arr) => {
-        return pizzaObj.id === pizzaId
-    })
 
-    res.json(result)
+// PUT /pizzas/:pizzaId -- Update one pizza
+app.put("/pizzas/:pizzaId", (req, res, next) => {
+
+    const { pizzaId } = req.params
+
+    const newDetails = req.body
+
+    Pizza.findByIdAndUpdate(pizzaId, newDetails, {new: true})
+        .then((pizzaFromDB) => {
+            res.json(pizzaFromDB)
+        })
+        .catch((error) => {
+            console.error("Error updating pizza...");
+            console.error(error);
+            res.status(500).json({ error: "Failed to update a pizza" });
+        })
+
+})
+
+
+// DELETE /pizzas/:pizzaId -- Delete one pizza
+app.delete("/pizzas/:pizzaId", (req, res, next) => {
+
+    const { pizzaId } = req.params
+
+    Pizza.findByIdAndDelete(pizzaId)
+        .then((response) => {
+            res.json(response)
+        })
+        .catch((error) => {
+            console.error("Error deleting pizza...");
+            console.error(error);
+            res.status(500).json({ error: "Failed to delete a pizza" });
+        })
 })
 
 
